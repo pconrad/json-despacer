9cf71d205af4acea4c6bbf9f4dc361aa127f7e1c
Phill Conrad <pconrad@cs.ucsb.edu>
2016-11-11 15:03:53 -0800

    WIP: successfully reading from JSON file

--- README.md
+++ README.md
@@ -11,6 +11,21 @@ To run, use `java -jar target/spark-template-mustache-2.4-SNAPSHOT.jar`
 
 Demo on Heroku at: https://sparkjava-corgis-airlines-demo.herokuapp.com/
 
+# JSON documentation
+
+The JSON API used here is json-simple.
+
+It appears that the current version of this in found in the following repo, but I'm not 100% confident
+this is the authoritative source:
+
+* https://github.com/fangyidong/json-simple
+
+
+Javadoc for it can be found at several places, though again it isn't clear which, if any, is an authoritative
+source; these appear to be volunteer efforts that could disappear at any time:
+
+* http://alex-public-doc.s3.amazonaws.com/json_simple-1.1/index.html
+* http://juliusdavies.ca/json-simple-1.1.1-javadocs/index.html?overview-summary.html
 
 # Test of Corgis ReadJson
 
--- src/main/java/org/pconrad/corgis/airlines/demos/ReadJson.java
+++ src/main/java/org/pconrad/corgis/airlines/demos/ReadJson.java
@@ -1,27 +1,51 @@
 package org.pconrad.corgis.airlines.demos;
 
-import java.util.ArrayList;
 import corgis.airlines.AirlinesLibrary;
 import corgis.airlines.domain.Airline;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class ReadJson {
-
+    
     public static void readJson(String fileName) {
-	//read file into stream, try-with-resources
-	try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-	    stream.forEach(System.out::println);
+
+	JSONParser parser = new JSONParser();
+
+	try {
+
+	    Object obj = parser.parse(new FileReader(fileName));
+
+	    for (Object o : (JSONArray) obj) {
+		Airline a = new Airline((JSONObject) o);
+		System.out.println("a=" + a);
+	    }
+
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
 	} catch (IOException e) {
 	    e.printStackTrace();
+	} catch (ParseException e) {
+	    e.printStackTrace();
 	}
+	
     }
-    
+
+    	
     public static void main(String[] args) {
 	readJson("airlines.json");
     }
+
 }
+
