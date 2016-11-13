2f3d92a7662eee4254b451179e78320fc42a7c34
Phill Conrad <pconrad@cs.ucsb.edu>
2016-11-13 15:44:03 -0800

    AirlinePlus spacing

--- src/main/java/org/pconrad/corgis/airlines/model/AirlinePlus.java
+++ src/main/java/org/pconrad/corgis/airlines/model/AirlinePlus.java
@@ -1,42 +1,26 @@
 package org.pconrad.corgis.airlines.model;
 
-import com.mongodb.Block;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-
-import static com.mongodb.client.model.Filters.eq;
-
-
 import corgis.airlines.domain.Airline;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-
-import org.bson.Document;
-
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import org.pconrad.utilities.EnvVars;
 
 public class AirlinePlus extends Airline {
     public AirlinePlus (JSONObject jsonObject) {
 	super(jsonObject);
     }
 
-    public String getMonth() { return "month"; }
-    public String getOnTimePercentage() { return "-"; }
+    public String getMonth() {
+	return String.format("%4d/%02d",
+			     this.getTime().getYear(),
+			     this.getTime().getMonth());
+    }
+    
+    public String getOnTimePercentage() {
+	int onTime = this.getStatistics().getFlights().getOnTime();
+	int total = this.getStatistics().getFlights().getTotal();
+	if (total==0)
+	    return "-";
+	return String.format("%5.2f", (100.0 * onTime)/total);
+    }
     
 }
 
