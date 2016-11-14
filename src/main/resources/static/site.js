3704eb21e3a5f73c096d3aacefcb4b1413abe75a
Phill Conrad <pconrad@cs.ucsb.edu>
2016-11-13 16:26:49 -0800

    table

--- src/main/resources/static/site.js
+++ src/main/resources/static/site.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $('#results-table').DataTable();
+    $('#results-table').dataTable( {
+	"lengthMenu": [ [10, 25, 50, 100, 1000, -1], [10, 25, 50, 100, 1000, "All"]],
+	
+    });
 });
+
+
